188468ebfb705b0704c472b1230b09da97aa5d5b
Wylie Conlon <wylieconlon@gmail.com>
2018-10-15 15:36:45 -0400

    Fix fizzbuzz sample

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      code: loopSample,
+      code: fizzbuzz,
       selectedSampleIndex: 0,
       loggedEvents: [],
       isShowingSource: false,
